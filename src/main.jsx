bbce9804c903ef76c8b803dd3a812b1839099dd5
mercie-ux <mbaomercy0@gmail.com>
2025-09-11 08:02:26 +0300

    removed theme context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './context/ThemeContext.jsx'
 import './index.css'
 import App from './App.jsx'
 import "./styles/global.css";
@@ -9,9 +8,7 @@ import "./styles/global.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
